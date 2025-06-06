73ab3762475bb49469c109ff6213ade94f482f12
R Z <your.email@example.com>
2025-06-06 16:50:13 -0400

    deleted hello world

--- script.js
+++ script.js
@@ -43,8 +43,8 @@ document.addEventListener("DOMContentLoaded", () => {
         '<span class="typewriter-chinese">世界, 你好</span>',
         /*"MARHABA BIL-3ĀLAM!",
         "OLÁ MUNDO!",
-        "M'ACH RHAESHESER!"*/
-        "HALLO, WERELD",
+        "M'ACH RHAESHESER!"
+        "HALLO, WERELD",*/
         "KALTXÌ, MA EYWA"
     ];
     const typeElem = document.getElementById("typewriter-text");
